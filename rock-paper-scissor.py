a673178edbe52368060fa8a8243a75fab6256ca1
Prajakta-glitch <guhaprajakta319@gmail.com>
2026-03-03 20:56:22 +0530

    Add files via upload

--- rock-paper-scissor.py
+++ rock-paper-scissor.py
@@ -1,54 +1,29 @@
-import tkinter as tk
-from tkinter import messagebox
 import random
 
 score=0
-total_game=0
 
-choose=["rock","paper","scissor"]
-print("choose from: ",choose)
-def play(your_choice):
-    global score, total_game
-    
-    user_text.set(f"You chose: {your_choice}")
-        
-    computer_choice=random.choice(choose)
-    comp_text.set(f"Computer chose: {computer_choice}")
-    
-    if(your_choice==computer_choice):
-        result.set("It's a tie!!!")
-    elif(your_choice=="rock" and computer_choice=="scissor")or(your_choice=="paper" and computer_choice=="rock")or(your_choice=="scissor" and computer_choice=="paper"):
-        result.set("You win!!!")
+while(1):
+    options=["rock","paper","scissor"]
+    print("Your options:",options)
+    user=input("Choose from the given options:").lower()
+
+    computer=random.choice(options)
+    print("Computer chose:",computer)
+
+    if user not in options:
+        print("Choose from the given options!")
+        print("Your score:",score)
+    elif((computer=="rock" and user=="paper")or(computer=="paper" and user=="scissor")or(computer=="scissor" and user=="rock")):
+        print("You win!")
         score+=1
+        print("Your score:",score)
+    elif(computer==user):
+        print("Draw!")
+        print("Your score:",score)
     else:
-        result.set("You Lose!!!")
-        
-    total_game+=1
-    score_text.set(f"{score}/{total_game}")
+        print("You Lose!")
+        print("Your score:",score)
     
-    play_again=messagebox.askyesno("Do you want to play again?", f"You chose {your_choice}\nComputer chose {computer_choice}\n\nPlay again?")
-    if not play_again:
-        root.destroy()
-
-root=tk.Tk()
-root.geometry("300x300")
-root.title("Rock Paper Scissor")
-
-user_text=tk.StringVar()
-result=tk.StringVar()
-score_text=tk.StringVar(value="0/0")
-comp_text=tk.StringVar()
-
-tk.Label(root, text="Choose one: ",font=("Arial",12)).pack(pady=5)
-
-tk.Button(root, text="Rock", width=12, command=lambda: play("rock")).pack(pady=4)
-tk.Button(root, text="Paper", width=12, command=lambda: play("paper")).pack(pady=4)
-tk.Button(root, text="Scisssor", width=12, command=lambda: play("scissor")).pack(pady=4)
-
-tk.Label(root, textvariable=user_text).pack(pady=5)
-tk.Label(root, textvariable=comp_text, font=("Arial", 10)).pack(pady=5)
-tk.Label(root, textvariable=result, font=("Arial", 12, "bold")).pack(pady=5)
-tk.Label(root, text="Score: ", font=("Arial", 10)).pack()
-tk.Label(root,textvariable=score_text, font=("Arial", 11)).pack()
-
-root.mainloop()
+    choice=int(input("Do you want to continue? If yes Press 1 else 0: "))
+    if(choice==0):
+        break
